4237988eeb02b480c373e191b863e1ad63b02148
wkuMyeungsu <pms9158@wku.ac.kr>
2026-07-22 20:25:25 +0900

    feat(cctv): DetectorManager에 메타데이터 전송(SendMetadata) 경로 연결

--- cctv/ArUCoDetector/app/src/DetectorManager/includes/detector_manager.h
+++ cctv/ArUCoDetector/app/src/DetectorManager/includes/detector_manager.h
@@ -2,6 +2,7 @@
 
 #include "component.h"
 #include "i_detector_manager.h"
+#include "metadata_format.h"
 
 class DetectorManager : public Component, public IDetectorManager {
  public:
@@ -17,6 +18,8 @@ class DetectorManager : public Component, public IDetectorManager {
   bool HandleHttpRequest(Event* event);
   void RegisterURI();
   std::string GetCurrentTimeToString();
+  void SendMetadata(const std::vector<int>& ids, const std::vector<std::vector<cv::Point2f>>& corners);
+  void ProcessMetadata(Event* event);
 
  private:
   std::string setting_changed_time_;
